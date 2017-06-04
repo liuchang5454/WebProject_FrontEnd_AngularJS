8b163adb0cb9a22900d0b11d3a4c3d154d604de3
liuchang5454 <liuchang5454@gmail.com>
2017-06-03 21:45:59 -0600

    week4 part3 Restful service and angualr . replaced module  with . all the pages works well excepts the index home page, likely because its code is not udpated for the featured / promotion / leadership

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -6,19 +6,20 @@ angular.module('confusionApp')
             
             $scope.showMenu = false;
             $scope.message = "Loading ...";
-            
-            $scope.dishes= {};
-            menuFactory.getDishes()
-            .then(
+            $scope.dishes = menuFactory.getDishes().query();
+
+            $scope.showMenu = false;
+            $scope.message = "Loading ...";
+            menuFactory.getDishes().query(
                 function(response) {
-                    $scope.dishes = response.data;
+                    $scope.dishes = response;
                     $scope.showMenu = true;
                 },
                 function(response) {
                     $scope.message = "Error: "+response.status + " " + response.statusText;
                 }
             );
-
+            
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
@@ -83,15 +84,13 @@ angular.module('confusionApp')
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-            $scope.dish = {};
             $scope.showDish = false;
             $scope.message="Loading ...";
-            
-            menuFactory.getDish(parseInt($stateParams.id,10))
-            .then(
+            $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)})
+            .$promise.then(
                 function(response){
-                    $scope.dish = response.data;
-                    $scope.showDish=true;
+                    $scope.dish = response;
+                    $scope.showDish = true;
                 },
                 function(response) {
                     $scope.message = "Error: "+response.status + " " + response.statusText;
@@ -101,34 +100,46 @@ angular.module('confusionApp')
             
         }])
 
-        .controller('DishCommentController', ['$scope', function($scope) {
+        .controller('DishCommentController', ['$scope', 'menuFactory', function($scope, menuFactory) {
             
             $scope.mycomment = {rating:5, comment:"", author:"", date:""};
-            
+
             $scope.submitComment = function () {
-                
                 $scope.mycomment.date = new Date().toISOString();
                 console.log($scope.mycomment);
-                
                 $scope.dish.comments.push($scope.mycomment);
-                
+
+                menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
                 $scope.commentForm.$setPristine();
-                
                 $scope.mycomment = {rating:5, comment:"", author:"", date:""};
             };
         }])
 
         // implement the IndexController and About Controller here
         .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory) {
-
-            $scope.dish = {};
-            $scope.showDish = false;
+/*
+            $scope.showDish = true;
             $scope.message="Loading ...";
+            $scope.dish = menuFactory.getDishes().get({id:0});
+
 
             menuFactory.getDish(0)
             .then(
                 function(response){
                     $scope.dish = response.data;
+                    $scope.showDish = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+            );
+*/
+            $scope.showDish = false;
+            $scope.message="Loading ...";
+            $scope.dish = menuFactory.getDishes().get({id:0})
+            .$promise.then(
+                function(response){
+                    $scope.dish = response;
                     $scope.showDish = true;
                 },
                 function(response) {
